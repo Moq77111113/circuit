2a46f27514aa8bde0b21870083117037bd20b6b1
Quentin Moessner <quentin.moessner@gmail.com>
2025-12-09 17:47:49 +0100

    wip: remove outdated comps

--- internal/ui/inputs/input_slice.go
+++ internal/ui/inputs/input_slice.go
@@ -2,14 +2,18 @@ package inputs
 
 import (
 	"fmt"
-	"reflect"
 
 	"github.com/moq77111113/circuit/internal/tags"
+	"github.com/moq77111113/circuit/internal/ui/components/containers"
 	g "maragu.dev/gomponents"
 	h "maragu.dev/gomponents/html"
 )
 
 func Slice(field tags.Field, value any) g.Node {
+	return SliceWithDepth(field, value, 0)
+}
+
+func SliceWithDepth(field tags.Field, value any, depth int) g.Node {
 	items := reflectSliceValues(value)
 
 	var nodes []g.Node
@@ -21,7 +25,7 @@ func Slice(field tags.Field, value any) g.Node {
 		))
 	} else {
 		for i, item := range items {
-			nodes = append(nodes, renderSliceItem(field, i, item))
+			nodes = append(nodes, containers.RenderSliceItem(field, i, item, depth))
 		}
 	}
 
@@ -33,108 +37,8 @@ func Slice(field tags.Field, value any) g.Node {
 		g.Text("Add Item"),
 	))
 
-	return h.Div(h.Class("slice"), g.Group(nodes))
-}
+	header := containers.CollapsibleHeader(field.Name, len(items), containers.IsCollapsed(depth))
+	body := containers.CollapsibleBody(nodes)
 
-func renderSliceItem(field tags.Field, index int, value any) g.Node {
-	var content g.Node
-
-	if len(field.Fields) > 0 {
-		content = renderStructItem(field, index, value)
-	} else {
-		content = renderPrimitiveItem(field, index, value)
-	}
-
-	return h.Div(
-		h.Class("slice__item"),
-		content,
-		h.Button(
-			h.Type("submit"),
-			h.Name("action"),
-			h.Value(fmt.Sprintf("remove:%s:%d", field.Name, index)),
-			h.Class("slice__remove-button"),
-			g.Text("Remove"),
-		),
-	)
-}
-
-func renderPrimitiveItem(field tags.Field, index int, value any) g.Node {
-	indexedName := fmt.Sprintf("%s.%d", field.Name, index)
-	indexedField := tags.Field{
-		Name:      indexedName,
-		Type:      field.ElementType,
-		InputType: field.InputType,
-		Required:  field.Required,
-		Min:       field.Min,
-		Max:       field.Max,
-		Step:      field.Step,
-	}
-
-	switch field.InputType {
-	case tags.TypeText:
-		return Text(indexedField, value)
-	case tags.TypeNumber:
-		return Number(indexedField, value)
-	case tags.TypeCheckbox:
-		return Checkbox(indexedField, value)
-	default:
-		return Text(indexedField, value)
-	}
-}
-
-func renderStructItem(field tags.Field, index int, value any) g.Node {
-	var fields []g.Node
-	for _, subfield := range field.Fields {
-		indexedName := fmt.Sprintf("%s.%d.%s", field.Name, index, subfield.Name)
-		indexedField := subfield
-		indexedField.Name = indexedName
-
-		subval := extractStructField(value, subfield.Name)
-
-		var input g.Node
-		switch subfield.InputType {
-		case tags.TypeText:
-			input = Text(indexedField, subval)
-		case tags.TypeNumber:
-			input = Number(indexedField, subval)
-		case tags.TypeCheckbox:
-			input = Checkbox(indexedField, subval)
-		default:
-			input = Text(indexedField, subval)
-		}
-
-		fields = append(fields, h.Div(
-			h.Class("slice__struct-field"),
-			h.Label(
-				h.For(indexedName),
-				h.Class("slice__struct-label"),
-				g.Text(subfield.Name),
-			),
-			input,
-		))
-	}
-
-	return h.Div(h.Class("slice__struct"), g.Group(fields))
-}
-
-func extractStructField(v any, fieldName string) any {
-	if v == nil {
-		return nil
-	}
-
-	rv := reflect.ValueOf(v)
-	if rv.Kind() == reflect.Pointer {
-		rv = rv.Elem()
-	}
-
-	if rv.Kind() != reflect.Struct {
-		return nil
-	}
-
-	fv := rv.FieldByName(fieldName)
-	if !fv.IsValid() {
-		return nil
-	}
-
-	return fv.Interface()
+	return containers.CollapsibleContainer(depth, header, body)
 }
--- internal/ui/inputs/section.go
+++ internal/ui/inputs/section.go
@@ -5,14 +5,30 @@ import (
 	h "maragu.dev/gomponents/html"
 )
 
-func Section(title string, children []g.Node) g.Node {
+func Section(title string, children []g.Node, collapsible bool, collapsed bool) g.Node {
+	classes := "section"
+	if collapsed {
+		classes += " collapsed"
+	}
+
+	var header g.Node
+	if collapsible {
+		header = h.Div(
+			h.Class("section__header"),
+			g.Attr("onclick", "toggleCollapse(this)"),
+			h.Span(h.Class("section__chevron"), g.Text("▼")),
+			h.H3(h.Class("section__title"), g.Text(title)),
+		)
+	} else {
+		header = h.H3(h.Class("section__title"), g.Text(title))
+	}
+
+	body := h.Div(h.Class("section__body"), g.Group(children))
+
 	return h.Section(
 		h.ID("section-"+title),
-		h.Class("section"),
-		h.H3(h.Class("section__title"), g.Text(title)),
-		h.Div(
-			h.Class("section__content"),
-			g.Group(children),
-		),
+		h.Class(classes),
+		header,
+		body,
 	)
 }
